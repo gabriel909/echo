4f27da1020d161e97e4abe5fe3f12918347048a1
Gabriel Oliveira <gabriel90939@gmail.com>
2017-09-20 15:37:42 -0300

    password_digest

--- app/models/escola.rb
+++ app/models/escola.rb
@@ -1,6 +1,2 @@
 class Escola < ApplicationRecord
-  has_secure_password
-
-  validates_presence_of :email, :CNPJ, :nome, :password
-  validates_uniqueness_of :email
 end
--- db/migrate/20170920183713_create_escolas.rb
+++ db/migrate/20170920183713_create_escolas.rb
@@ -1,11 +1,11 @@
 class CreateEscolas < ActiveRecord::Migration[5.1]
   def change
     create_table :escolas do |t|
-      t.string :unidade
       t.string :nome
-      t.string :CNPJ
       t.string :email
-      t.string :password
+      t.string :CNPJ
+      t.string :unidade
+      t.string :password_digest
 
       t.timestamps
     end
