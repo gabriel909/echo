9906c9997756c73f42b9b3e648f3c38dea436043
Gabriel Oliveira <gabriel90939@gmail.com>
2017-09-20 15:43:18 -0300

    Escola model password_digest

---
+++ db/migrate/20170920184256_create_escolas.rb
@@ -0,0 +1,13 @@
+class CreateEscolas < ActiveRecord::Migration[5.1]
+  def change
+    create_table :escolas do |t|
+      t.string :nome
+      t.string :email
+      t.string :password_digest
+      t.string :CNPJ
+      t.string :unidade
+
+      t.timestamps
+    end
+  end
+end
