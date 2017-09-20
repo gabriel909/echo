bd34e9650366ae63144ea7f9074ab1082cd0d889
Gabriel Oliveira <gabriel90939@gmail.com>
2017-09-20 17:56:55 -0300

    Json chaves 2

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -1,6 +1,6 @@
 module Response
   def json_response(object, status = :ok)
     #render json: { object }, status: status
-    render json: {status: 'SUCCESS', message: 'Loaded articles', data: object}, status: :ok
+    render json: { status: 'SUCCESS', message: 'Loaded articles', data: object }, status: :ok
   end
 end
