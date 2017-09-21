5ba3c582e95a3035875137aed1fa5ed684d93783
Gabriel Oliveira <gabriel90939@gmail.com>
2017-09-21 13:51:56 -0300

    migration error

--- db/migrate/20170920192206_remove_password_from_escola.rb
+++
@@ -1,5 +0,0 @@
-class RemovePasswordFromEscola < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :escolas, :password, :string
-  end
-end
--- db/migrate/20170920192310_add_password_digest_to_escola.rb
+++
@@ -1,5 +0,0 @@
-class AddPasswordDigestToEscola < ActiveRecord::Migration[5.1]
-  def change
-    add_column :escolas, :password_digest, :string
-  end
-end
