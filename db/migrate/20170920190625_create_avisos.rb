68a9a14ec8bcf07641ae3a214d6cc3e92cd06a0a
Gabriel Oliveira <gabriel90939@gmail.com>
2017-09-26 21:33:07 -0300

    Migration commented

--- db/migrate/20170920190625_create_avisos.rb
+++ db/migrate/20170920190625_create_avisos.rb
@@ -1,9 +1,9 @@
 class CreateAvisos < ActiveRecord::Migration[5.1]
   def change
-    create_table :avisos do |t|
-      t.string "titulo"
-      t.string "series"
-      t.text "descricao"
-    end
+    # create_table :avisos do |t|
+    #   t.string "titulo"
+    #   t.string "series"
+    #   t.text "descricao"
+    # end
   end
 end
